7d05844014c7319e209f20b310021dc9dd2929cc
SayaliMarathe-13 <marathesayali2003@gmail.com>
2025-05-19 18:57:23 -0700

    Added Models and Dal for BookIssue

---
+++ LMSClassLibrary/Models/BookIssueDetailsModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LMSClassLibrary.Models
+{
+    public class BookIssueDetailsModel
+    {
+        public int BookIssueDetailsId { get; set; }
+        public int BookIssueId { get; set; }
+        public int BookId { get; set; }
+        public bool IsActive { get; set; }
+        public string CreatedBy { get; set; }
+        public DateTime CreatedOn { get; set; }
+    }
+}
---
+++ LMSClassLibrary/Models/BookIssueModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LMSClassLibrary.Models
+{
+    public class BookIssueModel
+    {
+        public int BookIssueId { get; set; }
+        public int MemberId { get; set; }
+        public DateTime IssueDate { get; set; }
+        public DateTime DueDate { get; set; }
+        public DateTime? ReturnDate { get; set; }
+        public int LibrarianId { get; set; }
+        public bool IsActive { get; set; }
+        public string CreatedBy { get; set; }
+        public DateTime CreatedOn { get; set; }
+
+        //public List<BookIssueDetailModel> BookIssueDetails { get; set; }
+    }
+}
